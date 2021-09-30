edde5c1a83605f4067e4419a8db685105432be64
snehal-iblinfotech <snehal.chaudhari@iblinfotech.com>
2021-09-30 11:01:37 +0530

    added sttic file path

--- index.js
+++ index.js
@@ -9,6 +9,14 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors({ origin: true, credentials: true }));
 app.use("/api/auth", require("./Routes/auth.js"));
 app.use("/api/users", require("./Routes/users.js"));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (res, req) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 mongoose.connect(
   "mongodb+srv://root:root123@cluster0.4wkwj.mongodb.net/adminTable?retryWrites=true&w=majority",
   console.log("database connected"),
